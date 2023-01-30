fa4d5e0ccfbb195e1d34d56d32588c461aacdb35
SEONMl <sm000117@naver.com>
2023-01-31 00:14:56 +0900

    refactor: 지역 변수를 'in line' 하여 코드 압축, 가독성 향상

--- chap02/test/iloveyouboss/ProfileTest.java
+++ chap02/test/iloveyouboss/ProfileTest.java
@@ -19,32 +19,20 @@ class ProfileTest {
 
     @Test
     public void matchAnswersFalseWhenMustMatchCriteriaNotMet() {
-        Answer profileAnswer = new Answer(question, Bool.FALSE);
-        profile.add(profileAnswer);
-
-        Answer criteriaAnswer = new Answer(question, Bool.TRUE);
-        Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
-
-        criteria.add(criterion);
+        profile.add(new Answer(question, Bool.FALSE));
+        criteria.add(new Criterion(new Answer(question, Bool.TRUE), Weight.MustMatch));
 
         boolean matches = profile.matches(criteria);
 
         assertFalse(matches);
-        // 유지보수가 힘든 테스트
     }
     @Test
-    public void matchAnswersTrueForAnyDontcare() {
-        Answer profileAnswer = new Answer(question, Bool.FALSE);
-        profile.add(profileAnswer);
-
-        Answer criteriaAnswer = new Answer(question, Bool.TRUE);
-        Criterion criterion = new Criterion(criteriaAnswer, Weight.DontCare);
-
-        criteria.add(criterion);
+    public void matchAnswersTrueForAnyDontCareCriteria() {
+        profile.add(new Answer(question, Bool.FALSE));
+        criteria.add(new Criterion(new Answer(question, Bool.TRUE), Weight.DontCare));
 
         boolean matches = profile.matches(criteria);
 
         assertTrue(matches);
-        // 유지보수가 힘든 테스트
     }
 }
