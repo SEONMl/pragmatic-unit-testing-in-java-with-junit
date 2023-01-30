a0591698ad1c67aa2185e0cd34d665055d0e7b8d
SEONMl <sm000117@naver.com>
2023-01-31 00:25:22 +0900

    feat: 예제 용 테스트 객체 Account 생성

---
+++ chap03/src/scratch/Account.java
@@ -0,0 +1,24 @@
+package scratch;
+
+public class Account {
+    private int balance;
+    private String name;
+
+    public Account(String name) {
+        this.balance = 0;
+        this.name = name;
+    }
+
+    public void deposit(int money) {
+        this.balance += money;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public boolean hasPositiveBalance() {
+        if(balance>0) return true;
+        return false;
+    }
+}
