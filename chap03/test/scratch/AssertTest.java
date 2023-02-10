cd08cccb351b7bff9688e95f5b6f71ef9066fde5
SEONMl <sm000117@naver.com>
2023-02-10 23:29:39 +0900

    test: 부동소수점을 비교하는 테스트 코드

--- chap03/test/scratch/AssertTest.java
+++ chap03/test/scratch/AssertTest.java
@@ -3,6 +3,7 @@ package scratch;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AssertTest {
     @Test
@@ -11,5 +12,11 @@ public class AssertTest {
         // JUnit4 에 있다가 JUnit5로 오면서 빠진 라이브러리
         // 아래와 같은 사용법
         // assertEquals(new String[] {"1","2"}, equalTo(new String[] {"1","2"}));
+    }
+
+    @Test
+    public void assertHamcrestTest() {
+        assertTrue(Math.abs((2.32 * 3) - 6.96)<0.0005);
+        // assertEquals(2.32 * 3, closeTo(6.96, 0.0005));
     }
 }
