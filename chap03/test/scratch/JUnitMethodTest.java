4854d6c03337c331e9070f1676302debc92c0062
SEONMl <sm000117@naver.com>
2023-02-10 23:17:57 +0900

    test: assertEquals 메서드 테스트 코드 추가

--- chap03/test/scratch/JUnitMethodTest.java
+++ chap03/test/scratch/JUnitMethodTest.java
@@ -3,6 +3,7 @@ package scratch;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class JUnitMethodTest {
@@ -18,6 +19,19 @@ public class JUnitMethodTest {
     public void hasPositiveBalance(){
         account.deposit(50);
         assertTrue(account.hasPositiveBalance());
+    }
+
+    @Test
+    public void depositIncreaseBalance() {
+        int initialBalance = account.getBalance();
+        account.deposit(100);
+        assertTrue(account.getBalance() > initialBalance);
+    }
+
+    @Test
+    public void assertThatTest() {
+        account.deposit(100);
+        assertEquals(100, account.getBalance());
     }
 
 }
