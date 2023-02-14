a49eba7fe48d9a263263fe564e1ea82ffda944c1
SEONMl <sm000117@naver.com>
2023-02-14 16:07:56 +0900

    init chap12

---
+++ chap12/untitled/test/iloveyouboss/ProfileTest.java
@@ -0,0 +1,12 @@
+package iloveyouboss;
+
+import org.junit.jupiter.api.Test;
+
+class ProfileTest {
+
+    @Test
+    public void matchesNothingWhenProfileEmpty() {
+        new Profile();
+    }
+
+}
