b25fe467057b2e64bef4a50472f6264dd1e55673
SEONMl <sm000117@naver.com>
2023-02-14 15:07:47 +0900

    refactor: 의미있는 상수, 좋은 변수 이름, 좋은 데이터 등 테스트 상호 관련성 향상

--- chap11/test/util/SearchTest.java
+++ chap11/test/util/SearchTest.java
@@ -2,18 +2,15 @@ package util;
 
 
 import java.io.*;
-import java.net.*;
-
+import java.util.logging.*;
 import org.junit.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.logging.*;
-
-import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static util.ContainsMatches.*;
 
 public class SearchTest {
@@ -32,30 +29,24 @@ public class SearchTest {
 
     @Test
     public void returnsMatchesShowingContextWhenSearchStringInContent() {
-        stream = streamOn("There are certain queer times and occasions "
-                + "in this strange mixed affair we call life when a man "
-                + "takes this whole universe for a vast practical joke, "
-                + "though the wit thereof he but dimly discerns, and more "
-                + "than suspects that the joke is at nobody's expense but "
-                + "his own.");
-        Search search = new Search(stream, "practical joke", A_TITLE);
-        Search.LOGGER.setLevel(Level.OFF);
+        stream = streamOn("rest of text here"
+                + "1234567890search term1234567890"
+                + "more rest of text");
+        Search search = new Search(stream, "search term", A_TITLE);
         search.setSurroundingCharacterCount(10);
 
         search.execute();
 
         assertEquals(search.getMatches(), containsMatches(new Match[]
-                {new Match(A_TITLE, "practical joke",
-                        "or a vast practical joke, though t")}));
+                { new Match(A_TITLE,
+                        "search term",
+                        "1234567890search term1234567890") }));
     }
 
     @Test
-    public void noMatchesReturnedWhenSearchStringNotInContent()
-            throws MalformedURLException, IOException {
-        URLConnection connection =
-                new URL("http://bit.ly/15sYPA7").openConnection();
-        stream = connection.getInputStream();
-        Search search = new Search(stream, "smelt", A_TITLE);
+    public void noMatchesReturnedWhenSearchStringNotInContent() {
+        stream = streamOn("any text");
+        Search search = new Search(stream, "text that doesn't match", A_TITLE);
 
         search.execute();
 
