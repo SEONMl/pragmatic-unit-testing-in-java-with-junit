423636417bc58f4ce38fbf2d8f581e4eff5a02bc
SEONMl <sm000117@naver.com>
2023-02-14 12:54:25 +0900

    test: mock을 사용한 테스트

--- chap10/test/iloveyouboss/AddressRetrieverTest.java
+++ chap10/test/iloveyouboss/AddressRetrieverTest.java
@@ -8,48 +8,24 @@ import util.*;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class AddressRetrieverTest {
     @Test
     public void answersAppropriateAddressForValidCoordinates()
             throws IOException, ParseException {
-        // stub: 테스트 용도로 하드 코딩한 값을 반환하는 구현체
-        Http http = (String url) ->
+        // mock 인스턴스
+        Http http = mock(Http.class);
+        when(http.get(contains("lat=38.000000&lon=-104.000000"))) // 테스트 기대사항 설정
+                .thenReturn( // 기대사항이 충족되었을 때 처리
                 "{\"address\":{"
-                        + "\"house_number\":\"324\","
-                        + "\"road\":\"North Tejon Street\","
-                        + "\"city\":\"Colorado Springs\","
-                        + "\"state\":\"Colorado\","
-                        + "\"postcode\":\"80903\","
-                        + "\"country_code\":\"us\"}"
-                        + "}";
-        AddressRetriever retriever = new AddressRetriever(http);
-
-        Address address = retriever.retrieve(38.0,-104.0);
-
-        assertEquals(address.houseNumber, ("324"));
-        assertEquals(address.road, ("North Tejon Street"));
-        assertEquals(address.city, ("Colorado Springs"));
-        assertEquals(address.state, ("Colorado"));
-        assertEquals(address.zip, ("80903"));
-    }
-
-    @Test
-    public void returnsAppropriateAddressForValidCoordinates()
-            throws IOException, ParseException {
-        Http http = new Http() {
-            @Override
-            public String get(String url) throws IOException {
-                return "{\"address\":{"
-                        + "\"house_number\":\"324\","
-                        + "\"road\":\"North Tejon Street\","
-                        // ...
-                        + "\"city\":\"Colorado Springs\","
-                        + "\"state\":\"Colorado\","
-                        + "\"postcode\":\"80903\","
-                        + "\"country_code\":\"us\"}"
-                        + "}";
-            }};
+                + "\"house_number\":\"324\","
+                + "\"road\":\"North Tejon Street\","
+                + "\"city\":\"Colorado Springs\","
+                + "\"state\":\"Colorado\","
+                + "\"postcode\":\"80903\","
+                + "\"country_code\":\"us\"}"
+                + "}");
         AddressRetriever retriever = new AddressRetriever(http);
 
         Address address = retriever.retrieve(38.0,-104.0);
